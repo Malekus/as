f074b729aed6f3ee82bc4c71df2097c843451c5e
ChefDeProjet <abdelkader.moussa@outlook.fr>
2018-11-13 22:36:39 +0100

    Kmean ++ done

--- tp1.py
+++ tp1.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
+import numpy as np
 
 def e(x):
     return (x-1)*(x-2)*(x-3)*(x-5)
@@ -7,7 +9,7 @@ def De(x):
     return 4*x**3-33*x**2+82*x-61
 
 
-def DG(x0,mu, epsi=0.01, n_max=1000):
+def DG1(x0,mu, epsi=0.01, n_max=1000):
     x1 = x0-mu*De(x0)
     i = 0
     x = []
@@ -22,14 +24,14 @@ def DG(x0,mu, epsi=0.01, n_max=1000):
         y.append(i)
     return x1, i, e(x1), x, y
 
-
+"""
 a,b,c,data, label = DG(5,0.001)
 
 plt.figure()
 plt.plot(data, label)
 plt.show()
-
-
+"""
+"""
 def derivePa(a,b,x,y):
     return 2*x*(a*x+b-y)
     
@@ -38,7 +40,38 @@ def derivePb(a,b,x,y):
 
 def degA(i, n ,a, b, x, y):
     return degA(i-1, n ,a, b, x, y) - n * derivePa(a,b,x,y) * degA(i-1, n ,a, b, x, y)
-
-
-
 print(degA(5, 5,12,4,9,1))
+"""
+
+x, y = make_blobs(n_features=1, n_samples=100)
+
+def dp_a(a, n, x):
+    r = []
+    for xi in x:
+        r.append(2*xi[0])
+    return a - (n * np.array(r).sum())
+
+def dp_b(b, n, x):
+    return b - (n * 2*len(x))
+    
+def E(a,b,x,y):
+    r = []
+    for xi, yi in zip(x,y):
+        r.append(((a*xi[0]+b)-yi)**2)
+    return np.array(r).sum()
+
+def DG(a, b, x, y, n, nbMax):
+    r = []
+    r.append([a, b, E(a,b,x,y)])
+    for k in range(nbMax):
+        a = dp_a(a, n, x)
+        b = dp_b(b, n, x)
+        r.append([a, b, E(a,b,x,y)])
+    return np.array(r)
+
+print(DG(1, 1, x, y, 0.001, 100))
+print(DG(1, 1, x, y, 0.001, 500))
+print(DG(1, 1, x, y, 0.001, 1000))
+print(DG(1, 1, x, y, 0.01, 1000))
+print(DG(1, 1, x, y, 1, 1000))
+
---
+++ tp2.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.decomposition import PCA
+
+
+def getBarycenter(x, y, k):
+    if (y == k).sum():
+            return x[np.where(np.column_stack((x,y))[:,4] == k)].mean(0)
+    else :
+            return None
+        
+def k_mean(x, k):
+	d = x[np.random.choice(x.shape[0], k, replace=False)]
+	first = d
+	nb = []
+	while(1):
+		p = euclidean_distances(x, d)
+		label = np.argmin(p, axis=1)
+		d = np.array([getBarycenter(x, label, el) for el in range(k)])
+		nb.append(d.sum())
+		if len(nb) > 1 and nb[-2] == nb[-1] and nb[-3] == nb[-2]:
+			break
+	return np.array(label), np.array(nb), first
+
+def k_mean_plus(x, k):
+	d = x[np.random.choice(x.shape[0], 1, replace=False)]
+	for i in range(k - 1):
+		if i == 0:
+			d = np.append(d, [x[np.argmax(euclidean_distances(x,d))]], axis=0)
+		else:
+			d = np.append(d, [x[np.argmax(euclidean_distances(x,[d.mean(0)]))]], axis=0)
+	first = d
+	nb = []
+	while(1):
+		p = euclidean_distances(x, d)
+		label = np.argmin(p, axis=1)
+		d = np.array([getBarycenter(x, label, el) for el in range(k)])
+		nb.append(d.sum())
+		if len(nb) > 1 and nb[-2] == nb[-1] and nb[-3] == nb[-2]:
+			break
+	return np.array(label), np.array(nb), first
+
+x = datasets.load_iris().data
+y = datasets.load_iris().target
+pca = PCA(n_components=2)
+dataPCA = pca.fit_transform(x)
+kx, ky, fp = k_mean(x, 3)
+kxp, kyp, fpp = k_mean_plus(x, 3)
+
+plt.figure("Comparaison Kmean")
+plt.subplot(3,1,1)
+plt.title("No Kmean")
+plt.scatter(dataPCA[:,0], dataPCA[:, 1], c=y, alpha=0.8)
+plt.subplot(3,1,2)
+plt.title("Kmean")
+plt.scatter(dataPCA[:,0], dataPCA[:, 1], c=kx, alpha=0.8)
+plt.scatter(pca.fit_transform(fp)[0,0], pca.fit_transform(fp)[0,1], c='black', alpha=0.8, marker="X")
+plt.scatter(pca.fit_transform(fp)[1:,0], pca.fit_transform(fp)[1:,1], c='red', alpha=0.8)
+plt.subplot(3,1,3)
+plt.title("Kmean++")
+plt.scatter(dataPCA[:,0], dataPCA[:, 1], c=kxp, alpha=0.8)
+plt.scatter(pca.fit_transform(fpp)[0,0], pca.fit_transform(fpp)[0,1], c='black', alpha=0.8, marker="X")
+plt.scatter(pca.fit_transform(fpp)[1:,0], pca.fit_transform(fpp)[1:,1], c='red', alpha=0.8)
+plt.show()
