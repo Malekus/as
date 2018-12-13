e3aa09044a9058bc3326d763dafe0c0be37714ca
ChefDeProjet <abdelkader.moussa@outlook.fr>
2018-12-13 23:33:24 +0100

    fonction ++ -- rand

--- tp2.py
+++ tp2.py
@@ -49,6 +49,32 @@ dataPCA = pca.fit_transform(x)
 kx, ky, fp = k_mean(x, 3)
 kxp, kyp, fpp = k_mean_plus(x, 3)
 
+
+# Renvoie un tableau d'initialisation de taille K
+def initK(x, k, typeInit='Random'):
+    # Si on utilise l'initialisation ++
+    if typeInit is 'Plus':
+        d = x[np.random.choice(len(x), 1, replace=False)]
+        for i in range(k - 1):
+            if i == 0:
+                d = np.append(d, [x[np.argmax(euclidean_distances(x,d))]], axis=0)
+            else:
+                d = np.append(d, [x[np.argmax(euclidean_distances(x,[d.mean(0)]))]], axis=0)
+        return d
+    # Si on utilise l'initialisation --
+    if typeInit is 'Moins':
+        d = x[np.random.choice(len(x), 1, replace=False)]
+        for i in range(k - 1):
+            if i == 0:
+                d = np.append(d, [x[np.argmin(euclidean_distances(np.delete(x, d, axis=0),d))]], axis=0)
+            else:
+                d = np.append(d, [x[np.argmin(euclidean_distances(np.delete(x, d, axis=0),[d.mean(0)]))]], axis=0)
+        return d
+    # Sinon initilisation random de taille k
+    else:
+        return x[np.random.choice(len(x), 5, replace=False)]
+        
+"""
 plt.figure("Comparaison Kmean")
 plt.subplot(3,1,1)
 plt.title("No Kmean")
@@ -64,3 +90,6 @@ plt.scatter(dataPCA[:,0], dataPCA[:, 1], c=kxp, alpha=0.8)
 plt.scatter(pca.fit_transform(fpp)[0,0], pca.fit_transform(fpp)[0,1], c='black', alpha=0.8, marker="X")
 plt.scatter(pca.fit_transform(fpp)[1:,0], pca.fit_transform(fpp)[1:,1], c='red', alpha=0.8)
 plt.show()
+"""
+
+initK(x, 5, 'Plus')
